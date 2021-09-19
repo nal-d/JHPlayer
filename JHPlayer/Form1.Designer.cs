10359f32a25fcde2a867283dc80805e0c37ba73b
flyd3v <flyd3v@gmail.com>
2021-09-19 13:20:29 +0900

    초기 커밋.

---
+++ JHPlayer/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace JHPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.nSlider1 = new JHPlayer.NSlider();
+            this.watchaBtn = new System.Windows.Forms.Button();
+            this.primeBtn = new System.Windows.Forms.Button();
+            this.netflixBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.youtubeBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.BackColor = System.Drawing.Color.White;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(416, 156);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(75, 23);
+            this.webView.TabIndex = 3;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // nSlider1
+            // 
+            this.nSlider1.BackColor = System.Drawing.Color.Transparent;
+            this.nSlider1.BorderRoundRectSize = new System.Drawing.Size(8, 8);
+            this.nSlider1.DrawSemitransparentThumb = false;
+            this.nSlider1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F);
+            this.nSlider1.ForeColor = System.Drawing.Color.Black;
+            this.nSlider1.LargeChange = ((uint)(5u));
+            this.nSlider1.Location = new System.Drawing.Point(5, 2);
+            this.nSlider1.Minimum = 40;
+            this.nSlider1.Name = "nSlider1";
+            this.nSlider1.OverlayColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(190)))), ((int)(((byte)(190)))));
+            this.nSlider1.Size = new System.Drawing.Size(200, 28);
+            this.nSlider1.SmallChange = ((uint)(1u));
+            this.nSlider1.TabIndex = 2;
+            this.nSlider1.Text = "nSlider1";
+            this.nSlider1.ThumbRoundRectSize = new System.Drawing.Size(16, 16);
+            this.nSlider1.ThumbSize = new System.Drawing.Size(8, 16);
+            this.nSlider1.Value = 100;
+            this.nSlider1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.nSlider1_Scroll);
+            // 
+            // watchaBtn
+            // 
+            this.watchaBtn.BackColor = System.Drawing.Color.Transparent;
+            this.watchaBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.watchaBtn.FlatAppearance.BorderSize = 0;
+            this.watchaBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.watchaBtn.Image = global::JHPlayer.res.watcha;
+            this.watchaBtn.Location = new System.Drawing.Point(638, 400);
+            this.watchaBtn.Name = "watchaBtn";
+            this.watchaBtn.Size = new System.Drawing.Size(30, 30);
+            this.watchaBtn.TabIndex = 7;
+            this.watchaBtn.UseVisualStyleBackColor = false;
+            this.watchaBtn.Click += new System.EventHandler(this.watchaBtn_Click);
+            // 
+            // primeBtn
+            // 
+            this.primeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.primeBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.primeBtn.FlatAppearance.BorderSize = 0;
+            this.primeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.primeBtn.Image = global::JHPlayer.res.prime;
+            this.primeBtn.Location = new System.Drawing.Point(694, 358);
+            this.primeBtn.Name = "primeBtn";
+            this.primeBtn.Size = new System.Drawing.Size(30, 30);
+            this.primeBtn.TabIndex = 6;
+            this.primeBtn.UseVisualStyleBackColor = false;
+            this.primeBtn.Click += new System.EventHandler(this.primeBtn_Click);
+            // 
+            // netflixBtn
+            // 
+            this.netflixBtn.BackColor = System.Drawing.Color.Transparent;
+            this.netflixBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.netflixBtn.FlatAppearance.BorderSize = 0;
+            this.netflixBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.netflixBtn.Image = global::JHPlayer.res.netflix;
+            this.netflixBtn.Location = new System.Drawing.Point(576, 343);
+            this.netflixBtn.Name = "netflixBtn";
+            this.netflixBtn.Size = new System.Drawing.Size(30, 30);
+            this.netflixBtn.TabIndex = 5;
+            this.netflixBtn.UseVisualStyleBackColor = false;
+            this.netflixBtn.Click += new System.EventHandler(this.netflixBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.closeBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeBtn.FlatAppearance.BorderSize = 0;
+            this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeBtn.Image = global::JHPlayer.res.cancel;
+            this.closeBtn.Location = new System.Drawing.Point(767, -2);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(30, 30);
+            this.closeBtn.TabIndex = 4;
+            this.closeBtn.UseVisualStyleBackColor = false;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // youtubeBtn
+            // 
+            this.youtubeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.youtubeBtn.FlatAppearance.BorderSize = 0;
+            this.youtubeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.youtubeBtn.Image = global::JHPlayer.res.youtube;
+            this.youtubeBtn.Location = new System.Drawing.Point(605, 181);
+            this.youtubeBtn.Name = "youtubeBtn";
+            this.youtubeBtn.Size = new System.Drawing.Size(30, 30);
+            this.youtubeBtn.TabIndex = 8;
+            this.youtubeBtn.UseVisualStyleBackColor = false;
+            this.youtubeBtn.Click += new System.EventHandler(this.youtubeBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.ClientSize = new System.Drawing.Size(800, 482);
+            this.Controls.Add(this.youtubeBtn);
+            this.Controls.Add(this.watchaBtn);
+            this.Controls.Add(this.primeBtn);
+            this.Controls.Add(this.netflixBtn);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.nSlider1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "JHPlayer";
+            this.TopMost = true;
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private NSlider nSlider1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Button netflixBtn;
+        private System.Windows.Forms.Button primeBtn;
+        private System.Windows.Forms.Button watchaBtn;
+        private System.Windows.Forms.Button youtubeBtn;
+    }
+}
+
---
+++ JHPlayer/Form1.cs
@@ -0,0 +1,228 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JHPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.DoubleBuffered = true;
+            webView.Location = new Point(2, 32);
+            webView.DefaultBackgroundColor = Color.Transparent;
+            webView.Width = ClientSize.Width-4;
+            webView.Height = ClientSize.Height - 34;
+            this.SetStyle(ControlStyles.ResizeRedraw , true);
+            InitializeAsync();
+            
+        }
+
+
+        private async void InitializeAsync()
+        {
+
+            string _cacheFolderPath = System.IO.Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location, "\\cache");
+            var webView2Environment = await CoreWebView2Environment.CreateAsync(null, _cacheFolderPath);
+            await webView.EnsureCoreWebView2Async(webView2Environment);
+            webView.CoreWebView2.Navigate("https://netflix.com");
+        }
+        private void nSlider1_Scroll(object sender, ScrollEventArgs e)
+        {
+            Opacity = nSlider1.Value / 100.0;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            closeBtn.Location = new Point(ClientSize.Width - 31, 1);
+            watchaBtn.Location = new Point(ClientSize.Width - 61, 1);
+            primeBtn.Location = new Point(ClientSize.Width - 91, 1);
+            netflixBtn.Location = new Point(ClientSize.Width - 121, 1);
+            youtubeBtn.Location = new Point(ClientSize.Width - 151, 1);
+
+            webView.Width = ClientSize.Width - 4;
+            webView.Height = ClientSize.Height - 34;
+            Update();
+
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void netflixBtn_Click(object sender, EventArgs e)
+        {
+            webView.CoreWebView2.Navigate("https://netflix.com");
+        }
+
+        private void primeBtn_Click(object sender, EventArgs e)
+        {
+            webView.CoreWebView2.Navigate("https://www.primevideo.com/");
+        }
+
+        private void watchaBtn_Click(object sender, EventArgs e)
+        {
+            webView.CoreWebView2.Navigate("https://watcha.com");
+        }
+
+        private void youtubeBtn_Click(object sender, EventArgs e)
+        {
+            webView.CoreWebView2.Navigate("https://youtube.com");
+        }
+
+        private const int cCaption = 32;
+        private readonly ReSize resize = new ReSize();
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle bgrc = new Rectangle(2, 2, this.ClientSize.Width-4, ClientSize.Height-4);
+            Brush bg = new SolidBrush(Color.FromArgb(255, 255, 255, 255));
+            e.Graphics.FillRectangle(bg, bgrc);
+            Color theColor = Color.FromArgb(255, 150, 150, 150);
+            Brush brush = new SolidBrush(theColor);
+            Brush hBrush = new SolidBrush(Color.FromArgb(255, 190, 190, 190));
+            int BORDER_SIZE = 1;
+            ControlPaint.DrawBorder(e.Graphics, ClientRectangle,
+                                         theColor, BORDER_SIZE, ButtonBorderStyle.Solid,
+                                         theColor, BORDER_SIZE, ButtonBorderStyle.Solid,
+                                         theColor, BORDER_SIZE, ButtonBorderStyle.Solid,
+                                         theColor, BORDER_SIZE, ButtonBorderStyle.Solid);
+
+
+            Rectangle rc = new Rectangle(0, 0, this.ClientSize.Width, cCaption);
+            e.Graphics.FillRectangle(brush, rc);
+
+            if (btnIndex != -1)
+            {
+                e.Graphics.FillRectangle(hBrush, ClientSize.Width - (32 * btnIndex), 0, 32, 32);
+            }
+
+            base.OnPaint(e);
+
+
+        }
+        int btnIndex = -1;
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x84)
+            {  // Trap WM_NCHITTEST
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = this.PointToClient(pos);
+
+
+                switch (resize.GetMosuePosition(pos, this))
+                {
+                    case "l": m.Result = (IntPtr)10; return;  // the Mouse on Left Form
+                    case "r": m.Result = (IntPtr)11; return;  // the Mouse on Right Form
+                    case "a": m.Result = (IntPtr)12; return;
+                    case "la": m.Result = (IntPtr)13; return;
+                    case "ra": m.Result = (IntPtr)14; return;
+                    case "u": m.Result = (IntPtr)15; return;
+                    case "lu": m.Result = (IntPtr)16; return;
+                    case "ru": m.Result = (IntPtr)17; return; // the Mouse on Right_Under Form
+                    case "": m.Result = pos.Y < cCaption /*mouse on title Bar*/ ? (IntPtr)2 : (IntPtr)1; return;
+
+                }
+            }
+            else if (m.Msg == 0x20)
+            {  // Trap WM_SETCUROR
+                if ((m.LParam.ToInt32() & 0xffff) == 5)
+                { // Trap HTCAPTION
+                    Cursor.Current = Cursors.Hand;
+                    m.Result = (IntPtr)1;  // Processed
+                    return;
+                }
+                else if ((m.LParam.ToInt32() & 0xffff) == 1 || (m.LParam.ToInt32() & 0xffff) == 2)
+                {
+                    Cursor.Current = Cursors.Default;
+                    m.Result = (IntPtr)1;  // Processed
+                    return;
+                }
+            }
+            else if (m.Msg == 0xA2)
+            {
+                if (btnIndex == 1)
+                    Application.Exit();
+                else if (btnIndex == 2)
+                {
+                    webView.CoreWebView2.Navigate("https://watcha.com");
+                }
+                else if (btnIndex == 3)
+                {
+                    webView.CoreWebView2.Navigate("https://primevideo.com");
+                }
+                else if (btnIndex == 4)
+                {
+                    webView.CoreWebView2.Navigate("https://netflix.com");
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+
+        class ReSize
+        {
+
+            private bool Above, Right, Under, Left, Right_above, Right_under, Left_under, Left_above;
+            readonly int Thickness = 3;  //Thickness of border  u can cheang it
+            readonly int Area = 3;     //Thickness of Angle border 
+
+
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            /// <param name="thickness">set thickness of form border</param>
+            public ReSize(int thickness)
+            {
+                Thickness = thickness;
+            }
+
+            /// <summary>
+            /// Constructor set thickness of form border=1
+            /// </summary>
+            public ReSize()
+            {
+                Thickness = 2;
+            }
+
+            //Get Mouse Position
+            public string GetMosuePosition(Point mouse, Form form)
+            {
+                bool above_underArea = mouse.X > Area && mouse.X < form.ClientRectangle.Width - Area; /* |\AngleArea(Left_Above)\(=======above_underArea========)/AngleArea(Right_Above)/| */ //Area===>(==)
+                bool right_left_Area = mouse.Y > Area && mouse.Y < form.ClientRectangle.Height - Area;
+
+                bool _Above = mouse.Y <= Thickness;  //Mouse in Above All Area
+                bool _Right = mouse.X >= form.ClientRectangle.Width - Thickness;
+                bool _Under = mouse.Y >= form.ClientRectangle.Height - Thickness;
+                bool _Left = mouse.X <= Thickness;
+
+                Above = _Above && (above_underArea); if (Above) return "a";   /*Mouse in Above All Area WithOut Angle Area */
+                Right = _Right && (right_left_Area); if (Right) return "r";
+                Under = _Under && (above_underArea); if (Under) return "u";
+                Left = _Left && (right_left_Area); if (Left) return "l";
+
+
+                Right_above =/*Right*/ (_Right && (!right_left_Area)) && /*Above*/ (_Above && (!above_underArea)); if (Right_above) return "ra";     /*if Mouse  Right_above */
+                Right_under =/* Right*/((_Right) && (!right_left_Area)) && /*Under*/(_Under && (!above_underArea)); if (Right_under) return "ru";     //if Mouse  Right_under 
+                Left_under = /*Left*/((_Left) && (!right_left_Area)) && /*Under*/ (_Under && (!above_underArea)); if (Left_under) return "lu";      //if Mouse  Left_under
+                Left_above = /*Left*/((_Left) && (!right_left_Area)) && /*Above*/(_Above && (!above_underArea)); if (Left_above) return "la";      //if Mouse  Left_above
+
+                return "";
+
+            }
+
+
+        }
+
+    }
+}
